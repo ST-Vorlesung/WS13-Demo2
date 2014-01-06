973bade8c2174d81b9c6316ea66bb910b65deeeb
Peter Knauber <p.knauber@hs-mannheim.de>
2014-01-06 15:06:00 +0100

    Methode weg

--- ST1/src/p1/Hello.java
+++ ST1/src/p1/Hello.java
@@ -4,10 +4,7 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
-	}
-	
-	private void hello() {
 		System.out.println("signature");
 	}
-
+	
 }
